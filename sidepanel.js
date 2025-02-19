b98cba7178ed322934ff83dfa9a38cf55016caff
yeseung <hayeseung@gmail.com>
2025-02-19 16:55:03 +0900

    first commit

---
+++ sidepanel.js
@@ -0,0 +1,29 @@
+
+
+function openURLInNewTab(url) {
+    chrome.tabs.create({ url });
+}
+
+document.getElementById('click01').addEventListener('click', () => { openURLInNewTab("https://xn--o39az0a1b953fgwo.com/adm/") }, false);
+document.getElementById('click02').addEventListener('click', () => { openURLInNewTab("https://xn--2e0bob225cz4dgz6a.com/") }, false);
+document.getElementById('click03').addEventListener('click', () => { openURLInNewTab("https://comkijung.com/") }, false);
+document.getElementById('click04').addEventListener('click', () => { openURLInNewTab("https://www.youtube.com/channel/UCRslqvDMF8ZFdcvnrqAXL0g") }, false);
+document.getElementById('click05').addEventListener('click', () => { openURLInNewTab("https://xn--2e0bobz35eppszrcgxh.com/") }, false);
+document.getElementById('click06').addEventListener('click', () => { openURLInNewTab("https://smartstore.naver.com/ajjulcom") }, false);
+document.getElementById('click07').addEventListener('click', () => { openURLInNewTab("https://gunggumi.com/") }, false);
+document.getElementById('click08').addEventListener('click', () => { openURLInNewTab("https://xn--2e0baa5fq39anxnwubw2q8ik1xdh98a.com/") }, false);
+document.getElementById('click09').addEventListener('click', () => { openURLInNewTab("https://xn--2e0bobz35eppszrcgxh.com/%ec%88%98%eb%a6%ac%ec%84%9c%eb%b9%84%ec%8a%a4/") }, false);
+document.getElementById('click10').addEventListener('click', () => { openURLInNewTab("https://4594.kr/") }, false);
+document.getElementById('click11').addEventListener('click', () => { openURLInNewTab("https://xn--2e0bob864d15bq5x.com/") }, false);
+document.getElementById('click12').addEventListener('click', () => { openURLInNewTab("https://www.youtube.com/@%EA%B2%9C%EA%B8%B0%EC%A0%95Tv-v5k") }, false);
+document.getElementById('click13').addEventListener('click', () => { openURLInNewTab("https://e999999999999999.cafe24.com/adm") }, false);
+document.getElementById('click14').addEventListener('click', () => { openURLInNewTab("https://k999998889698576.cafe24.com/adm") }, false);
+document.getElementById('click15').addEventListener('click', () => { openURLInNewTab("https://xn--hs0bl3k.com/") }, false);
+document.getElementById('click16').addEventListener('click', () => { openURLInNewTab("http://webmail.gunggumi.com/") }, false);
+document.getElementById('click17').addEventListener('click', () => { openURLInNewTab("https://adviser85.synology.me:63744/") }, false);
+document.getElementById('click18').addEventListener('click', () => { openURLInNewTab("https://xn--o80b71uztc4kx9xxzojqc.com/") }, false);
+// document.getElementById('click19').addEventListener('click', () => { openURLInNewTab("") }, false);
+// document.getElementById('click20').addEventListener('click', () => { openURLInNewTab("") }, false);
+// document.getElementById('click21').addEventListener('click', () => { openURLInNewTab("") }, false);
+// document.getElementById('click22').addEventListener('click', () => { openURLInNewTab("") }, false);
+// document.getElementById('click23').addEventListener('click', () => { openURLInNewTab("") }, false);
